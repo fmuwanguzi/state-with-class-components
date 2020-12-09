9d059f00e61c8afd958d1b4b19d1fab23e1d8402
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-12-09 11:38:51 -0500

    add and subtract using input

--- src/App.js
+++ src/App.js
@@ -8,30 +8,40 @@ constructor(){
   super()
   this.state={
     count:0,
+    inputtedNumber: 0,
   }
 
 }
 
   increment = () => {
-    const currentCount = this.state.count
-    const newCount = currentCount + 1
+    //const amountToAdd = prompt('How much do you want to add?' )
     
-    this.setState({ count: newCount})
-
-  }
+    const newCount = this.state.count + this.state.inputtedNumber
+    // const newCount = currentCount + 1
+    
+     this.setState({ count: newCount})
+}
 
   decrement = () => {
+    //const amountToSub = prompt('How much do you want tosubtract? ')
+   
     // const currentCount = this.state.count
-    // const newCount = currentCount - 1
-    
-    this.setState({ count: this.state.count - 1 })
+    // const newCount = this.state.count - parseInt(amountToSub)
+    const newCount = this.state.count - this.state.inputtedNumber    
+    this.setState({ count: newCount })
+}
 
+  changeInput = (evt) => {
+    //console.log(evt.target.value)
+    let newValue = parseInt(evt.target.value) || 0
+    this.setState({inputtedNumber: newValue})
   }
 
   render() {
   return (
     <div>
       <h1> {this.state.count} </h1>
+      <input value={this.state.inputtedNumber} onChange={this.changeInput}/>
       <button onClick={this.increment}>+</button>
       <button onClick={this.decrement}>-</button>
     </div>
